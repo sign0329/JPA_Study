ce8aae7aad379c4eae68e3e8d6bd0a1ba200369b
sangminKim <s0329kim@gmail.com>
2024-10-12 19:24:00 +0900

    초기 설정

---
+++ src/main/java/com/ll/jpa_20241011/Jpa20241011Application.java
@@ -0,0 +1,13 @@
+package com.ll.jpa_20241011;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa20241011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa20241011Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/jpa_20241011/Jpa20241011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jpa_20241011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa20241011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
